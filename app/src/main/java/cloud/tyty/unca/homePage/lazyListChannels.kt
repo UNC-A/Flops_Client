79c97b58a641c7c9e03563c4da5b2751433b6931
Ash <ashy@tyty.cloud>
2024-02-24 11:57:33 +0800

    Mostly okay card design
    Pushing this now in case I need the old design

--- app/src/main/java/cloud/tyty/unca/homePage/lazyListChannels.kt
+++ app/src/main/java/cloud/tyty/unca/homePage/lazyListChannels.kt
@@ -1,6 +1,7 @@
 package cloud.tyty.unca.homePage
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,6 +25,7 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -39,20 +41,53 @@ import cloud.tyty.unca.timestampConversion
 fun lazyListChannels() {
     addDummyUsers()
     dummyUserList.sortBy { it.timestamp }
+
     LazyColumn(modifier = Modifier.fillMaxWidth(), content = {
         items(dummyUserList) { users ->
-            ListItem(
-                leadingContent = {
-                    Icon(users.icon, contentDescription = null, modifier = Modifier.size(36.dp))
-                },
-                headlineContent = { Text(users.name) },
-                supportingContent = { Text(users.lastMessage)},
-                trailingContent = { Text(timeStampLazyList(users.timestamp))}
+            Spacer(modifier = Modifier.padding(vertical = 1.dp))
 
-            )
+            Card()
+            {
+                Column(modifier = Modifier
+                    .fillMaxWidth()) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 15.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                            .fillMaxWidth(0.3f)
+                            .padding(start = 5.dp)
+                        )
+                        {
+
+                            Text(text = users.name, style = MaterialTheme.typography.titleSmall, maxLines = 2)
+                        }
+                        Spacer(Modifier.padding(horizontal = 8.dp))
+                        Column(modifier = Modifier
+                            .fillMaxWidth(0.8f)
+                            .padding(vertical = 24.dp))
+                        {
+                            Text(text = users.lastMessage, maxLines = 2)
+                        }
+                        Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+                        Text(text = timeStampLazyList(users.timestamp), style = MaterialTheme.typography.bodySmall, maxLines = 2)
+                    }
+                }
+            }
         }
     })
 }
+
+
+
+//@Preview
+//@Composable
+//fun cardDesign() {
+
+
 
 data class dummyUsers(
     val name: String, val timestamp: Long, val lastMessage: String, val icon: ImageVector
@@ -61,7 +96,7 @@ val dummyUserList = mutableListOf<dummyUsers>()
 fun addDummyUsers() {
     dummyUserList.add(
         dummyUsers(
-            "Kaia",
+            "Kaia Collett",
             timestamp = 1708576972,
             lastMessage = "How are you doing today? Remember, we have dinner tonight at 6!",
             icon = Icons.Default.AccountCircle
@@ -69,7 +104,7 @@ fun addDummyUsers() {
     )
     dummyUserList.add(
         dummyUsers(
-            "Ashy",
+            "Ash Thornborough",
             1708579723,
             "Hello, I am just writing this to test the effectiveness of the lazyColumn",
             Icons.Default.AccountCircle
@@ -78,41 +113,5 @@ fun addDummyUsers() {
 
 }
 
-@Composable
-fun cardDesign() {
-//    Card()
-//    {
-//        Column(modifier = Modifier
-//            .height(70.dp)
-//            .fillMaxWidth()) {
-//            Row(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(start = 10.dp),
-//                verticalAlignment = Alignment.CenterVertically,
-//                horizontalArrangement = Arrangement.Start
-//            ) {
-//                Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth(0.3f))
-//                {
-//
-//                    Icon(
-//                        imageVector = users.icon,
-//                        contentDescription = users.name,
-//                        modifier = Modifier.size(48.dp)
-//                    )
-//                    Spacer(modifier = Modifier.padding(horizontal = 5.dp))
-//                    Text(text = users.name, style = MaterialTheme.typography.labelLarge, maxLines = 2)
-//                }
-//                Spacer(Modifier.padding(horizontal = 15.dp))
-//                Column(modifier = Modifier
-//                    .fillMaxWidth(0.8f)
-//                    .fillMaxSize()
-//                    .padding(vertical = 18.dp))
-//                {
-//                    Text(text = users.lastMessage, maxLines = 2)
-//                }
-//                Text(text = timeStampLazyList(users.timestamp), style = MaterialTheme.typography.bodySmall, maxLines = 2)
-//            }
-//        }
-//    }
-}
+
+
