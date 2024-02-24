9920823e746dcdb90a35982959f648df65474c32
Ash <ashy@tyty.cloud>
2024-02-24 14:29:24 +0800

    init of RoomDB

---
+++ app/src/main/java/cloud/tyty/unca/database/Room.kt
@@ -0,0 +1,29 @@
+package cloud.tyty.unca.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.RoomDatabase
+import kotlinx.serialization.SerialName
+
+@Entity
+data class Messages(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "channel_id") val channelID: String,
+    @ColumnInfo(name = "message") val message: String,
+    @ColumnInfo(name = "is_sent") val isSent: Boolean
+)
+
+@Dao
+interface UserDao
+{
+    @Insert
+    fun insertMessage(vararg messages: Messages)
+}
+@Database(entities = [Messages::class], version = 1)
+abstract class MessagesDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
